c322735a4d4412bca6aa8beea7f8914d9479f675
Mike Roberts <mike@sdcs.io>
2018-11-06 10:54:58 -0800

    adds webpack, ejs, and template

--- server.js
+++ server.js
@@ -3,7 +3,12 @@ const app = express();
 
 const PORT = process.env.PORT || 3000;
 
-app.use(express.static('public'));
+
+app.set('view engine', 'ejs');
+app.get('/', function(req, res) {
+    res.render('index');
+});
+
 app.listen(PORT, function () {
     console.log(`We're live on port 3000!`);
 });
