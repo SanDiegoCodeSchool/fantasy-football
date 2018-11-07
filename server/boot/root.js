3c90a586d81a22a19ada983b94dc03e4c5473d37
Michael Roberts <michael@origincodeacademy.com>
2018-11-07 08:43:53 -0800

    fixes server to be the loopback server and removes old server.js, adjusts middleware

--- server/boot/root.js
+++ server/boot/root.js
@@ -4,5 +4,11 @@ module.exports = function(server) {
   // Install a `/` route that returns server status
   var router = server.loopback.Router();
   // router.get('/', server.loopback.status());
+
+  server.set('view engine', 'ejs');
+
+  server.get('/', function(req, res) {
+    res.render('index');
+  });
   server.use(router);
 };
